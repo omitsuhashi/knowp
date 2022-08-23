fe35a83275928ae67c686033403e0814cb4bcdca
Osamu Mitsuhashi <osamu.mitsuhashi224@gmail.com>
2022-08-24 00:37:25 +0900

    move home style file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,34 @@
-import type { NextPage } from 'next'
-import styles from '../styles/Home.module.scss'
-import Header from "../components/header";
+import type { NextPage } from 'next';
+import styles from './Index.module.scss';
+import Header from '../components/header';
+import { Grid, Typography } from '@mui/material';
 
 const Home: NextPage = () => {
   return (
-      <>
-        <div className={styles.header}>
-          <Header isLoggedIn={true} />
+    <>
+      <div className={styles.header}>
+        <Header isLoggedIn={true} />
+        {/* insert hero */}
+        <div className={styles.body}>
+          <Grid container spacing={4}>
+            <Grid item xs={10}>
+              <Typography>left side</Typography>
+            </Grid>
+            <Grid item sm container>
+              <Grid item xs container direction={'column'} spacing={1}>
+                <Grid item>
+                  <Typography>right top</Typography>
+                </Grid>
+                <Grid item>
+                  <Typography>right bottom</Typography>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
         </div>
-      </>
-  )
-}
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
