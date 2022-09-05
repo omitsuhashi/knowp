e8e7551a5644678295ec8524e5c9996cc955c4a0
Osamu Mitsuhashi <osamu.mitsuhashi224@gmail.com>
2022-09-06 00:51:15 +0900

    feat setup swr base

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { SWRConfig, SWRConfiguration } from "swr";
+import axios from "axios";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function KnowpApp({ Component, pageProps }: AppProps) {
+  const url = process.env.API_URL;
+
+  const fetcher = axios.create({
+    baseURL: url,
+  });
+
+  const swrConfig: SWRConfiguration = {
+    fetcher,
+  };
+  return (
+    <SWRConfig value={swrConfig}>
+      <Component {...pageProps} />
+    </SWRConfig>
+  );
 }
 
-export default MyApp
+export default KnowpApp;
