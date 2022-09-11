0a5c858982c694dea0fcb6c673b2c6147fbb2711
Osamu Mitsuhashi <osamu.mitsuhashi224@gmail.com>
2022-09-11 12:17:37 +0900

    fix set props to mediaCard

--- components/elements/media_card.tsx
+++ components/elements/media_card.tsx
@@ -4,35 +4,40 @@ import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 
 interface IProps {
-  query: { id: number };
+  query: {
+    imageUri: string;
+    alt?: string;
+    height?: string;
+    title: string;
+    subtitle: string;
+    content: JSX.Element;
+  };
 }
 
 const MediaCard: FC<IProps> = (props) => {
+  const {
+    imageUri,
+    alt = "",
+    height = "140",
+    title,
+    subtitle,
+    content,
+  } = props.query;
   return (
     <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        component="img"
-        height="140"
-        image="/static/images/cards/contemplative-reptile.jpg"
-        alt="green iguana"
-      />
+      <CardMedia component="img" height={height} image={imageUri} alt={alt} />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          Lizard {props.query.id}
+          {title}
         </Typography>
         <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
+          {subtitle}
         </Typography>
       </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
+      <CardActions>{content}</CardActions>
     </Card>
   );
 };
